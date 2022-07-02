c690ce0d6aadc994995891db298f0796054f8f86
AbuHurairah127 <abuhurairahMuhammadShafique@gmail.com>
2022-07-03 00:01:43 +0500

    ads section completed responsive

--- src/components/ads/Ads.js
+++ src/components/ads/Ads.js
@@ -2,15 +2,17 @@ import React from "react";
 import "./Ads.css";
 const Ads = () => {
   return (
-    <div className="container h-[300px] flex justify-evenly items-center">
+    <div className="ads-container h-[300px] flex flex-wrap justify-center md:justify-evenly items-center">
       <div className="flex flex-col">
-        <h1 className="text-white opacity text-3xl font-bold">
+        <h1 className="text-white text-center md:text-left opacity text-3xl font-bold">
           Free consultation with exceptional quality.
         </h1>
-        <p>Just one call away: +92 320 7443537</p>
+        <p className="text-center md:text-left ads-text">
+          Just one call away: +92 320 7443537
+        </p>
       </div>
       <div className="btn-container">
-        <button className="btn">Get your consultation</button>
+        <button className="button">Get your consultation</button>
       </div>
     </div>
   );
